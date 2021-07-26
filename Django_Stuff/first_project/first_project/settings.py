8a5fee81e7448dbb3c89dc875638e2bacec23c80
Omid Soroush <omidsoroush@t-online.de>
2021-07-26 16:20:06 +0200

    modified settings.py

--- Django_Stuff/first_project/first_project/settings.py
+++ Django_Stuff/first_project/first_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-czg0y=#1a0ryyary8j37*-d@x9nejpzak!__bnf&%b6!c7r90f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
