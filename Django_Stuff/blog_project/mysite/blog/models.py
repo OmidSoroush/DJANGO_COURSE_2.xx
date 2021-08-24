664a6460b6178248ea292d973f3b0ba1e9df9303
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-08-24 23:13:57 +0200

    blog project continued

--- Django_Stuff/blog_project/mysite/blog/models.py
+++ Django_Stuff/blog_project/mysite/blog/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
@@ -20,7 +20,7 @@ class Post(models.Model):
         return self.comments.filter(approved_comment=True)
 
     def get_absolute_url(self):
-        return reverse("post_detial", kwargs={'pd':self.pk})
+        return reverse("post_detail", kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.title
@@ -28,7 +28,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments')
+    post = models.ForeignKey('blog.Post', related_name='comments',on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
