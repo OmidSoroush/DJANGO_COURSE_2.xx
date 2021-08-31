e74bc6e142cbc14e96a7eea163700238d02c1959
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-08-31 12:42:54 +0200

    adv topics finished

--- Django_Stuff/adv_topics/my_video_rental/videos/admin.py
+++ Django_Stuff/adv_topics/my_video_rental/videos/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from . import models
+
+
+class MovieAdmin(admin.ModelAdmin):
+
+    fields = ['release_year', 'title', 'length']
+
+    search_fields = ['title', 'length']
+
+    list_filter = ['release_year', 'length', 'title']
+
+    list_display = ['release_year', 'length', 'title']
+
+    list_editable = ['length']
+
 # Register your models here.
 admin.site.register(models.Customer)
-admin.site.register(models.Movie)
+admin.site.register(models.Movie, MovieAdmin)
