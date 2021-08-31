2863d22c30f36e1e999865b74d12d457e3b24359
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-08-31 09:25:48 +0200

    ProTwo project added

--- Django_Stuff/ProTwo/AppTwo/urls.py
+++ Django_Stuff/ProTwo/AppTwo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from AppTwo import views
-
+from . import views
 
 urlpatterns = [
-    path('', views.help, name='help')
+    path('', views.index, name="index"),
+    path('users/', views.users, name="users"),
+    path('help/', views.help, name='help'),
 ]
--- Django_Stuff/ProTwo/AppTwo/views.py
+++ Django_Stuff/ProTwo/AppTwo/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
+
 # Create your views here.
-
-
-def index(request):
-    return HttpResponse("<em>My second project</em>")
+# def index(request):
+#     return HttpResponse("<em>My Second Project</em>")
 
 def help(request):
-    helpdict = {'help_insert': 'Help Page'}
-    return render(request, 'AppTwo/help.html', context= helpdict)
+    helpdict = {'help_insert':'HELP PAGE'}
+    return render(request,'appTwo/help.html',context=helpdict)
+
+def index(request):
+    return render(request,'appTwo/index.html')
+
+def users(request):
+    user_list = User.objects.order_by('first_name')
+    user_dict = {"users":user_list}
+    return render(request,'appTwo/users.html',context=user_dict)
